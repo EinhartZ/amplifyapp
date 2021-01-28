8b5a514dd640fcc209c415fdb40d6e52e9628b2e
zhang_einhart <einhart.zhang@qaelum.com>
2021-01-28 16:05:46 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	  
+	<Card>
+	  <Card.Header>Featured</Card.Header>
+	  <Card.Body>
+		<Card.Title>Special title treatment</Card.Title>
+		<Card.Text>
+		  With supporting text below as a natural lead-in to additional content.
+		</Card.Text>
+		<Button variant="primary">Go somewhere</Button>
+	  </Card.Body>
+	</Card>
+	
+	<Card>
+	  <Card.Header>Quote</Card.Header>
+	  <Card.Body>
+		<blockquote className="blockquote mb-0">
+		  <p>
+			{' '}
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+			erat a ante.{' '}
+		  </p>
+		  <footer className="blockquote-footer">
+			Someone famous in <cite title="Source Title">Source Title</cite>
+		  </footer>
+		</blockquote>
+	  </Card.Body>
+	</Card>
+	  
     </div>
   );
 }
